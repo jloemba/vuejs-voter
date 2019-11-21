0a91e01e1480e5b8380d2e59c25f6e820d22ce3f
jloemba <jorisloemba@gmail.com>
2019-11-21 17:42:11 +0100

    Fetching vote

---
+++ README.md
@@ -0,0 +1,11 @@
+# Installation 
+
+
+# Démarrage 
+ 
+ ```
+ docker-compose up -d
+ docker-compose run client
+ ```
+
+ Normalement , l'application est accessible sur localhost:8080
---
+++ client/src/components/vote/FetchVote.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <ul>
+
+            <li
+            is="todo-item"
+            v-for="l in list"
+            v-bind:key="l.ID"
+            v-bind:title="l.title"
+            >{{l.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+      name: 'FetchVote',
+      props:{
+          msg:String,
+          list:Array
+      },
+      methods:{
+            getVote: function() {
+               
+                Axios
+                .get('http://localhost:8081/api/vote/')
+                .then(response => {
+                        var data = response.data
+                        this.msg = data.message
+                        this.list = data.vote
+                })
+            }
+        },
+      beforeMount () {
+        this.getVote()   
+      }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
