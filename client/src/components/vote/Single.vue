5965abb8c7da301ae19e32c74345f5b0133ec781
louisdeluche <louis87@hotmail.fr>
2020-01-30 03:43:07 +0100

    correction derniere modif

--- client/src/components/vote/Single.vue
+++ client/src/components/vote/Single.vue
@@ -1,31 +1,9 @@
 <template>
     <div>
-        <h1>Information sur le sujet de vote </h1>
-
-        <div class="vote-card" v-if="vote">
-            <p>{{vote.title}}</p>
-            <p>{{vote.description}}</p>
-            <p>Début : {{vote.start_date | formatDate}} </p>
-            <p> Clôture :  {{vote.end_date | formatDate}} </p>
-            <PieChart v-bind:results="vote.uuid_votes" />
-            <!-- <Chart v-bind:artists="vote.uuid_votes" /> -->
-            <div v-if=" Date.now() > new Date(vote.end_date) ">
-                <p>Vous ne pouvez plus soumettre votre bulletin. Les votes sont clôs.</p>
-            </div>
-            <div v-else>
-                <div v-if="hasVoted()">
-                    <p><a v-bind:href="'/vote/submit/'+ vote.uuid">Soumettre votre vote </a></p>
-                </div>
-                <div v-else>
-                    <p>Vous avez soumis votre vote</p>
-                </div>
-                <div v-if="checkLevelAccess()">
-             <template>
-    <div>
     <h1>Information sur le sujet de vote </h1>
 
        <div class="vote-card" v-if="vote">
-            <p>{{vote.title}}</p>
+            <p>{{vote.title}}</p> 
             <p>{{vote.description}}</p>
             <p>Début : {{vote.start_date | formatDate}} </p>
             <p> Clôture :  {{vote.end_date | formatDate}} </p>
@@ -81,9 +59,8 @@ export default {
               .get('http://localhost:8081/api/vote/show/'+this.id)
               .then(response => {
                     if(response.data.status){
-
+                        
                         this.vote = response.data.vote
-
                         var arr = []
                         var voterID = []
                         for (let index = 0; index < response.data.vote.uuid_votes.length; index++) {
@@ -97,14 +74,14 @@ export default {
 
                     }else{
                         this.msg = response.data.message
-                    }
+                    }                   
                })
           },
           hasVoted:function(){ //pour vérifier si l'user a déjà voté
-
-
+                //if(this.voterList != undefined){
                     if(this.voterList.filter(el => el === localStorage.getItem('UUID') )) return true
                     else return false
+                //}else return true
           },
           checkLevelAccess(){
                 if(localStorage.getItem('access_level')){
@@ -113,15 +90,14 @@ export default {
                     else return false
 
                 }else{
-
+                    //console.log(false)
                     return false
                 }
           }
       },
       beforeMount(){
           this.id = this.$route.params.voteid
-          this.getDataVote()
-
+          this.getDataVote() 
       },
       updated(){
           this.hasVoted()
@@ -138,103 +114,5 @@ export default {
 .vote-card , h1{
     text-align :left;
 }
-
-</style>       <p><a v-bind:href="'/vote/update/'+ vote.uuid">Éditer ce sujet de vote </a></p>
-                    <p><a v-bind:href="'/vote/delete/'+ vote.uuid">Supprimer ce sujet de vote </a></p>
-                </div>
-            </div>
-
-        </div>
-        <div v-else>
-            <span>{{msg}}</span>
-        </div>
-
-    </div>
-</template>
-
-<script>
-    //import Chart from './chart'
-    import Axios from 'axios'
-    import PieChart from './PieChart'
-
-    export default {
-        name: 'Single',
-        components:{
-            PieChart
-        },
-        props:{
-            id:String,
-            vote:{},
-            msg:String,
-            voterList:Array
-        },
-        creted(){
-            this.results = []
-        },
-        methods:{
-            getDataVote() {
-
-                Axios
-                    .get('http://localhost:8081/api/vote/show/'+this.id)
-                    .then(response => {
-                        if(response.data.status){
-
-                            this.vote = response.data.vote
-                            console.log(this.vote)
-                            var arr = []
-                            var voterID = []
-                            for (let index = 0; index < response.data.vote.uuid_votes.length; index++) {
-                                voterID.push(response.data.vote.uuid_votes[index].split('||')[0])
-                                arr.push(response.data.vote.uuid_votes[index].split('||')[1])
-                            }
-
-                            this.voterList = voterID
-                            this.vote.uuid_votes = arr
-                            this.results = this.vote.uuid_votes
-
-                        }else{
-                            this.msg = response.data.message
-                        }
-                    })
-            },
-            hasVoted:function(){ //pour vérifier si l'user a déjà voté
-                //if(this.voterList != undefined){
-                console.log(this.voterList)
-                if(this.voterList.filter(el => el === localStorage.getItem('UUID') )) return true
-                else return false
-                //}else return true
-            },
-            checkLevelAccess(){
-                if(localStorage.getItem('access_level')){
-                    //console.log(true)
-                    if(localStorage.getItem('access_level') == 2) return true
-                    else return false
-
-                }else{
-                    //console.log(false)
-                    return false
-                }
-            }
-        },
-        beforeMount(){
-            this.id = this.$route.params.voteid
-            this.getDataVote()
-            console.log(this.uuid_votes)
-        },
-        updated(){
-            this.hasVoted()
-            this.results = this.vote.uuid_votes
-        },
-        beforeUpdate(){
-            this.results = this.vote.uuid_votes
-        }
-    }
-</script>
-
-<style scoped>
-
-    .vote-card , h1{
-        text-align :left;
-    }
 
 </style>
